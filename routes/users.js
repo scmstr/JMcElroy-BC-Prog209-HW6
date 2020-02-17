bbf2a4eec8784964e2e568eea37a47acb173df13
scmstr <55764330+scmstr@users.noreply.github.com>
2020-02-16 21:46:15 -0800

    Delete users.js

--- routes/users.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users page. */
-router.get('/', function (req, res) {
-    res.render('users', { title: 'users' });
-});
-
-module.exports = router;
