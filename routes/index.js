1d54f87eba2bce353f7158cd7c7eb38e4b8da5ab
scmstr <55764330+scmstr@users.noreply.github.com>
2020-02-16 21:46:08 -0800

    Delete index.js

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET helo world (HOME) page. */
-router.get('/', function(req, res, next) {
-    res.render('helloWorld', { title: 'Hello World!' });
-});
-
-module.exports = router;
